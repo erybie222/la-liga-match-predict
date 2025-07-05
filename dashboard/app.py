833932ec1c33160ea659af7214e0a8175a35a5de
Eryk Bielawski <erybie222@gmail.com>
2025-07-05 10:24:47 +0200

    feat: ELO ranking

--- dashboard/app.py
+++ dashboard/app.py
@@ -2,11 +2,14 @@ import csv
 from datetime import datetime
 import sys
 import os
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from features.elo import compute_elo_ratings
 import streamlit as st
 import pandas as pd
 import joblib
 from utils.preprocessing import prepare_match_features
+from utils.elo_utils import get_current_elo_ranking
 
 model = joblib.load('../models/best_model_tuned_smote.pkl')
 label_encoder = joblib.load('../models/label_encoder_smote.pkl')
@@ -14,13 +17,48 @@ label_encoder = joblib.load('../models/label_encoder_smote.pkl')
 df = pd.read_csv(os.path.join(os.path.dirname(__file__), '..', 'data', 'LaLiga_Matches.csv'), parse_dates=['Date'])
 teams = sorted(set(df['HomeTeam']) | set(df["AwayTeam"]))
 
+
+
+with st.sidebar:
+    view = st.radio("📊 Choose view:", ["🧠 Match prediction", "📈 ELO ranking"])
+
+if view == "📈 ELO ranking":
+    st.title("📈 ELO ranking of La Liga teams")
+    df_ranking = get_current_elo_ranking()
+    st.dataframe(df_ranking, use_container_width=True)
+
+    selected_team = st.selectbox("📍 Choos team to analyze the change of ELO in timeline", df_ranking['Team'])
+
+    df_all = pd.read_csv(os.path.join(os.path.dirname(__file__), '..', 'data', 'LaLiga_Matches.csv'), parse_dates=['Date'])
+    df_all = df_all.sort_values('Date')
+    df_all = compute_elo_ratings(df_all)
+
+    elo_values = []
+    dates = []
+
+    for _, row in df_all.iterrows():
+        if row['HomeTeam'] == selected_team:
+            elo_values.append(row['home_elo'])
+            dates.append(row['Date'])
+        elif row['AwayTeam'] == selected_team:
+            elo_values.append(row['away_elo'])
+            dates.append(row['Date'])
+
+    df_plot = pd.DataFrame({'Date': pd.to_datetime(dates, dayfirst=True), 'ELO': elo_values})
+    df_plot = df_plot.sort_values('Date')  
+    st.line_chart(df_plot.set_index('Date'))
+
+
 st.title("⚽ La Liga Match Predictor")
+
 history_path = os.path.join(os.path.dirname(__file__), '..', 'history', 'predictions.csv')
 if os.path.exists(history_path):
     df_history = pd.read_csv(history_path)
     st.dataframe(df_history.tail(5).iloc[::-1],use_container_width=True)
 else:
     st.info("No predictions yet.")
+
+
 home = st.selectbox("🏠 Choose home team", teams)
 away = st.selectbox("🚗 Choose away team", teams)
 
